fde0db16fe5987c07163edadaa58f2c79d4397a8
LoV432 <monib619@gmail.com>
2024-08-21 20:09:03 +0500

    add prettier

--- lib/db.ts
+++ lib/db.ts
@@ -3,36 +3,38 @@ import mongoose from 'mongoose';
 const MONGODB_URI = process.env.MONGODB_URI as string;
 
 if (!MONGODB_URI) {
-  throw new Error(
-    'Please define the MONGODB_URI environment variable inside .env.local'
-  );
+	throw new Error(
+		'Please define the MONGODB_URI environment variable inside .env.local'
+	);
 }
 
-
 //@ts-ignore
-let cached = global.mongoose as { conn: null | mongoose.Mongoose, promise: null | Promise<mongoose.Mongoose> };
+let cached = global.mongoose as {
+	conn: null | mongoose.Mongoose;
+	promise: null | Promise<mongoose.Mongoose>;
+};
 
 if (!cached) {
-    //@ts-ignore
-  cached = global.mongoose = { conn: null, promise: null };
+	//@ts-ignore
+	cached = global.mongoose = { conn: null, promise: null };
 }
 
 async function dbConnect() {
-  if (cached.conn) {
-    return cached.conn;
-  }
+	if (cached.conn) {
+		return cached.conn;
+	}
 
-  if (!cached.promise) {
-    const opts = {
-      bufferCommands: false,
-    };
+	if (!cached.promise) {
+		const opts = {
+			bufferCommands: false
+		};
 
-    cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
-      return mongoose;
-    });
-  }
-  cached.conn = await cached.promise;
-  return cached.conn;
+		cached.promise = mongoose.connect(MONGODB_URI, opts).then((mongoose) => {
+			return mongoose;
+		});
+	}
+	cached.conn = await cached.promise;
+	return cached.conn;
 }
 
 export default dbConnect;
